b8f2dd0426c4345a1f9cd5f2cffe40463131b7b3
Paul Johnson <paule.johnson@gmail.com>
2021-12-16 20:59:21 -0700

    real good progress, got some debugging to do starting with the current sample input

--- day16/solutions.py
+++ day16/solutions.py
@@ -35,8 +35,46 @@ def hexToBin(line):
     }
     return ''.join([hexToBinDict[c] for c in line])
 
+# you have to guarantee the line starts with a packet.
+def extractPackets(line):
+    allPackets = []
+    version = line[:3]
+    type = line[3:6]
+    packet = version + type # yes i know i just separated them leave me alone
+    print(version, type, packet, line)
+    if type == '100':
+        i = 6
+        digits = 'a' # obvs wrong initial value
+        while digits[0] != '0':
+            digits = line[i:i+5]
+            packet += digits
+            i += 5
+        allPackets.append(packet)
+    else:
+        I = line[6]
+        packet += I
+        if I == '0':
+            # based on total subpacket length
+            packetLength = binaryStringToDecimal(line[7:22])
+            subPackets = []
+            while sum(map(len, subPackets)) < packetLength:
+                subPackets.extend(extractPackets(line[22+sum(map(len, subPackets)):22+packetLength]))
+            allPackets.append(line[:22+packetLength])
+            allPackets.extend(subPackets)
+        if I == '1':
+            # based on count of subpackets
+            packetCount = binaryStringToDecimal(line[7:18])
+            subPackets = []
+            while len(subPackets) < packetCount:
+                subPackets.extend(extractPackets(line[18+sum(map(len, subPackets)):]))
+            allPackets.append(line[:18+sum(map(len, subPackets))])
+            allPackets.extend(subPackets)
+    return allPackets
+
 def part1(input):
-    return None
+    packets = extractPackets(hexToBin(input[0]))
+    pp.pprint(packets)
+    return sum([binaryStringToDecimal(line[:3]) for line in packets])
 
 def part2(input):
     return None
